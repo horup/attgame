c5a9464b260173b6738634c6bea52d2c9c072bc4
Horup <skivehus@gmail.com>
2019-09-15 20:15:06 +0200

    updated

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import * as Phaser from 'phaser';
 import { State } from './state';
 declare var require;
 
+export class Unit extends Phaser.GameObjects.Sprite
+{
+    player:number = 0;
+}
+
 export class AttScene extends Phaser.Scene
 {
     state:State;
@@ -23,40 +28,87 @@ export class AttScene extends Phaser.Scene
 
     tilemap:Phaser.Tilemaps.Tilemap;
     last = {x:0, y:0};
+
+    units:Unit[] = [];
+    selectedUnit:Phaser.GameObjects.Sprite;
+    selectionBox:Phaser.GameObjects.Image;
+
+    makeUnit(x:number, y:number, player = 0)
+    {
+        let u = new Unit(this, x, y, 'units', 0);
+        u.player = player;
+        this.children.add(u);
+        u.setInteractive();
+        this.units.push(u);
+
+        return u;
+    }
+
+    selectUnit(u:Unit)
+    {
+        this.selectedUnit = u;
+    }
+
     create()
     {
+        this.game.canvas.oncontextmenu = (e)=>e.preventDefault();
+        
         this.state = new State();
         let state = this.state;
         state.initDemo();
-
-
-        this.make.tilemap();
-
-
-        let g = this.make.group({});
 
         this.tilemap = this.make.tilemap({tileHeight:16, tileWidth:16, width:256, height:256});
         this.tilemap.addTilesetImage('tiles');
         this.tilemap.addTilesetImage('units');
         this.tilemap.addTilesetImage('overlay');
-        this.tilemap.createBlankDynamicLayer("ground", 'tiles');
+        this.tilemap.createBlankDynamicLayer("ground", 'tiles').setInteractive().on('pointerdown', (e:PointerEvent)=>
+        {
+            console.log("test");
+            if (e.button == 0)
+                this.selectedUnit = null;
+            else if (e.button == 2)
+            {
+                if (this.selectedUnit != null)
+                {
+                    this.selectedUnit.x = e.x;
+                    this.selectedUnit.y = e.y;
+                }
+            }
+        })
         this.tilemap.createBlankDynamicLayer("unit", 'units');
         this.tilemap.createBlankDynamicLayer("overlay", 'overlay');
-
-
         this.tilemap.setLayer("ground");
         let size = 32;
         for (let y = 0; y < size; y++)
             for (let x = 0; x < size; x++)
                 this.tilemap.putTileAt(11 + Phaser.Math.RND.integer() % 2, x, y);
 
-        this.tilemap.setLayer("unit");
+        {
+            let u = this.makeUnit(100, 100);
+            u.on('pointerdown', (e:PointerEvent)=>e.button == 0 ? this.selectUnit(u) : undefined);
+        }
+
+        {
+            let u = this.makeUnit(100, 400, 1);
+            u.on('pointerdown', (e:PointerEvent)=>e.button == 0 ? this.selectUnit(u) : undefined);
+        }
+
+
+        this.selectionBox = this.make.image({key:'overlay', x:16, y:16, frame:2});
     }
 
 
     update()
     {
-        let state = this.state;
+        for (let u of this.units)
+        {
+            u.setFrame(u.player);
+        }
+        if (this.selectedUnit != null)
+            this.selectionBox.setVisible(true).setPosition(this.selectedUnit.x, this.selectedUnit.y);
+        else
+            this.selectionBox.setVisible(false);
+                /*   let state = this.state;
         let tilemap = this.tilemap;
 
 
@@ -80,7 +132,7 @@ export class AttScene extends Phaser.Scene
         {
             let u = state.units[id];
             tilemap.putTileAt(0, u.x, u.y);
-        }
+        }*/
     }
 }
 
@@ -96,6 +148,7 @@ export class AttGame extends Phaser.Game
             parent: 'AttGame',
             scene: [AttScene]
         });
+
     }
 }
 
